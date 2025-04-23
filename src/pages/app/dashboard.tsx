aa4372f32eab270491b46e040f1e10d5ea730be4
gisellenrosa <gisellenrosa@gmail.com.br>
2025-04-22 21:53:00 -0300

    fix: adjusts on dashboard

--- src/pages/app/dashboard.tsx
+++ src/pages/app/dashboard.tsx
@@ -1,24 +1,7 @@
 import { LineChartComponent } from "@/components/recharts";
 import { Card, CardContent, CardTitle } from "@/components/ui/card";
-import { CartesianGrid, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
-
-
-
 
 export function Dashboard()  {
-
-  const chartsData = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400},]
-
- const renderLineChart = (
-  <LineChart width={600} height={300} data={chartsData} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
-    <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-    <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-    <XAxis dataKey="name" />
-    <YAxis />
-    <Tooltip />
-  </LineChart>
-);
-
 const data = [
   {
     id: 1,
